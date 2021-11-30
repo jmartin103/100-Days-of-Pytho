dd37ed60ce46689ffa58bdb72ec946cb8fd9829a
jmartin103 <jwmartin1222@gmail.com>
2021-11-30 08:33:32 -0500

    Day 8

---
+++ Day 8/SilentAuction.py
@@ -0,0 +1,25 @@
+def find_highest_bidder(bidders):
+    highest_bid = 0
+    highest_bidder = ''
+    for key in bidders:
+        if bidders[key] > highest_bid:
+            highest_bidder = key
+            highest_bid = bidders[key]
+    
+    print(f'The winner is {highest_bidder} with a bid of ${highest_bid}')
+    return highest_bid
+
+def main():
+    bidders = {}
+    other_bidders = 'yes'
+    while other_bidders == 'yes':
+        name = input('Enter the bidder\'s name: ')
+        bid = int(input('How much would you like to bid? $'))
+        bidders[name] = bid
+
+        other_bidders = input('Are there any other bidders? ').lower()
+        if other_bidders == 'no':
+            find_highest_bidder(bidders)
+            break
+
+main()
